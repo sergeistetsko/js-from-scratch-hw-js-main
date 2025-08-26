0c1cf943b842322e947aaef925f3ecf8660665b1
sergeistetsko <sergeistetsko2021@yandex.by>
2025-08-26 08:43:16 +0300

    task 3 final

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,11 +1,5 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(firstNumber, secondNumber, thirdNumber) {
-  if (firstNumber >= secondNumber && secondNumber >= thirdNumber) {
-    return firstNumber
-  } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
-    return secondNumber
-  } else {
-    return thirdNumber
-  }
+  return Math.max(firstNumber, secondNumber, thirdNumber)
 }
