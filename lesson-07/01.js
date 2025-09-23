9c1ec9e30488d0a481e8887d8db07c65041b9fe4
sergeistetsko <sergeistetsko2021@yandex.by>
2025-09-23 11:23:25 +0300

    complete task

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,27 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+const imageElement = document.getElementById('web-tech-image')
+
+let currentIndex = 0
+imageElement.src = WEB_TECH_IMAGES[currentIndex]
+
+nextButton.addEventListener('click', () => {
+  currentIndex = currentIndex + 1
+  if (currentIndex === WEB_TECH_IMAGES.length) {
+    currentIndex = 0
+  }
+  imageElement.src = WEB_TECH_IMAGES[currentIndex]
+})
+
+prevButton.addEventListener('click', () => {
+  if (currentIndex === 0) {
+    currentIndex = WEB_TECH_IMAGES.length - 1
+  } else {
+    currentIndex = currentIndex - 1
+  }
+  imageElement.src = WEB_TECH_IMAGES[currentIndex]
+})
